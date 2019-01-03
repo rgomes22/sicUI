eafeb7fbc3783626bbd7cc20e87717bdac5eebd8
David Guilherme <1160684@isep.ipp.pt>
2019-01-03 23:07:34 +0000

    cabide adicionado

---
+++ src/app/Component/three/three-files/Hanger.ts
@@ -0,0 +1,36 @@
+import * as THREE from 'three';
+import { Scene } from 'three';
+import {Obj3D} from './interfaces/Obj3D';
+import { dependenciesFromGlobalMetadata } from '@angular/compiler/src/render3/r3_factory';
+
+export class Hanger implements Obj3D{
+    private hanger : THREE.Mesh;
+
+
+    constructor(handlerRadius : number, length : number){
+        let shelfMaterial = new THREE.MeshPhongMaterial({color: 0x559a99});
+        let shelfGeometry = new THREE.CylinderGeometry(handlerRadius,handlerRadius,length,32);
+        
+        this.hanger  = new THREE.Mesh(shelfGeometry,shelfMaterial);
+
+        this.hanger.receiveShadow=true;
+        this.hanger.castShadow=true;
+        
+    }
+
+    mesh(){
+        return this.hanger; 
+    }
+
+    position(vector: THREE.Vector3) {
+        this.hanger.position.set(vector.x,vector.y,vector.z);
+    }
+
+    rotate(angle: number, vector: THREE.Vector3) {
+        this.hanger.rotateOnAxis(vector,angle);
+    }
+
+    public addShelfToScene(scene:Scene){
+        scene.add(this.hanger);
+    }
+}
